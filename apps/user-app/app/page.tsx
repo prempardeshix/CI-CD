74a73aa3810b055fc8416584e583bce568de4536
Prem Pardeshi <prempardeshi283@gmail.com>
2024-07-24 22:48:21 +0530

    bkl

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -3,7 +3,13 @@ import { redirect } from "next/navigation";
 import { authOptions } from "./lib/auth";
 
 export default async function Page() {
+
+
+  
   console.log("hello");
+
+
+
   const session = await getServerSession(authOptions);
   if (session?.user) {
     redirect("/dashboard");
